547d8fffae9a9ac9a29941290678fb1c166d0928
Christopher Robertson <christopherprobertson@gmail.com>
2020-01-31 21:53:28 +0000

    - Started production on the PlayerMovement.cs with a snapping function and a function to get the player input from the keyboard.

--- global-gamejam-project/Assets/blank-project/Scripts/PlayerMovement.cs
+++ global-gamejam-project/Assets/blank-project/Scripts/PlayerMovement.cs
@@ -1,6 +1,39 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Physics")]
+    private Rigidbody rigidBody;
 
+    [Header("Position")]
+    [SerializeField]
+    private float snapValue;
+    private Vector3 nextPosition;
+
+    [Header("Input")]
+    [SerializeField]
+    private string moveXName;
+    private float moveX;
+    [SerializeField]
+    private string moveZName;
+    private float moveZ;
+
+    private void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    private void GetPlayerInput()
+    {
+        moveX = Input.GetAxis(moveXName);
+        moveZ = Input.GetAxis(moveZName);
+    }
+
+    private Vector3 RoundVector(Vector3 vector, float snap)
+    {
+        return new Vector3((vector.x / snap) * snap,
+                           (vector.y / snap) * snap,
+                           (vector.z / snap) * snap);
+    }
 }
