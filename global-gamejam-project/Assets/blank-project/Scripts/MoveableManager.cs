059eec62b4a4274926daf6b11fd168531f0e7eaa
MP232027 <MP232027@falmouth.ac.uk>
2020-02-01 11:49:53 +0000

    - Initial implementation of pickup mechanic, key down is detected but primary function is not complete. - MoveableObject class is used to identify objects that can be picked up. - Added Player prefab and Plank_MAT material.

---
+++ global-gamejam-project/Assets/blank-project/Scripts/MoveableManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableManager : MonoBehaviour
+{
+    // [SerializeField]
+    // private GameObject player;
+    [SerializeField]
+    private GameObject slot;
+    [SerializeField]
+    private GameObject container;
+    private GameObject holding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Key down", gameObject);
+            if(holding == null)
+            {
+                Pickup();
+            } else
+            {
+                Drop();
+            }
+        }
+    }
+
+    void Pickup()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(gameObject.transform.position, Vector3.forward, out hit, 5f))
+        {
+            if (hit.collider.GetComponent<MoveableObject>() != null)
+            {
+                Debug.Log("Raycast", hit.collider.gameObject);
+                hit.collider.transform.SetParent(slot.transform);
+                holding = hit.collider.gameObject;
+            }
+        }
+    }
+
+    void Drop()
+    {
+        holding.transform.position = gameObject.transform.forward;
+        holding.transform.SetParent(container.transform);
+        holding = null;
+    }
+}
